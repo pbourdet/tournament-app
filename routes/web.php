56787a4d0a0657e7cb5e17ca9f177f4bdf1e9c15
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-04-13 12:04:29 +0200

    Deleted home page and redirect guests to /login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect('/dashboard') : redirect('/login');
 });
 
 Route::get('/dashboard', function () {
