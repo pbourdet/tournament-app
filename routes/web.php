d497259da86a37ba368bd9f69e277955e4edcffa
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-04-26 18:06:13 +0200

    Display tournaments on dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Account\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return auth()->check() ? redirect('/dashboard') : redirect('/login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
