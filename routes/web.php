e49cf9e9048e39de78c81f96ef7e0f2129cbe102
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-05-17 20:41:55 +0200

    Added route to get the form for joining a tournament

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::post('/tournaments/{tournament}/invitation/create', [TournamentInvitationController::class, 'store'])->name('tournament_invitation.create');
+    Route::get('/tournaments/{code}/join', [TournamentController::class, 'show'])->name('tournament.invitation');
     Route::post('/tournaments/{tournament}/join', [TournamentController::class, 'join'])->name('tournament.join');
 });
 
