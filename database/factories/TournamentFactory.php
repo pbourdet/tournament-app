0e5d9c49ab080999f84317bf30a4f0d349b96151
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-05-17 14:25:35 +0200

    Created join tournament route

--- database/factories/TournamentFactory.php
+++ database/factories/TournamentFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\Tournament;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /** @extends Factory<Tournament> */
@@ -16,6 +17,7 @@ class TournamentFactory extends Factory
         return [
             'name' => fake()->words(asText: true),
             'description' => fake()->text(),
+            'organizer_id' => User::factory()->create()->id,
             'number_of_players' => fake()->numberBetween(1, 5) * 2,
         ];
     }
