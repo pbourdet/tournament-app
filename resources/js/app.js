44031b8d0a8f654de4f7a24e9ce139426854cb0a
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-05-19 19:09:42 +0200

    Installed htmx and jQuery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,19 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import htmx from 'htmx.org';
 
 import.meta.glob([
     '../images/**',
 ]);
 
 window.Alpine = Alpine;
+window.htmx = htmx
+
+if (import.meta.env.VITE_INCLUDE_JQUERY === 'true') {
+    import('jquery').then(({ default: $ }) => {
+        window.jQuery = $;
+    });
+}
 
 Alpine.start();
