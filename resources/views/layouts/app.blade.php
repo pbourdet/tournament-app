520640d42ac96677801eb967d17bcf655651d101
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2024-06-04 18:36:31 +0200

    Improved layout file and created guest navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,41 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.base')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@section('body-class', 'font-sans antialiased')
+@section('container-class', 'min-h-screen bg-gray-100')
+@section('content')
+    <!-- Page Heading -->
+    @if (isset($header))
+        <header class="bg-white shadow">
+            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                {{ $header }}
+            </div>
+        </header>
+    @endif
 
-        <!-- Favicon -->
-        <link rel="icon" type="image/x-icon" href="favicon.ico?v=1" />
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <x-toast-container/>
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main class="py-12">
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
-</html>
+    <!-- Page Content -->
+    <main class="py-12">
+        {{ $slot }}
+    </main>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="favicon.ico?v=1"/>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="@yield('body-class', 'font-sans antialiased')">
+        @guest
+            @include('layouts.navigation-guest')
+        @endguest
+        @auth
+            @include('layouts.navigation')
+        @endauth
+        <x-toast-container/>
+        <div class="@yield('container-class', 'min-h-screen bg-gray-100')">
+            @yield('content')
+        </div>
+    </body>
+</html>
