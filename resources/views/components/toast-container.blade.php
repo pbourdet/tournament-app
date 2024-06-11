75f67ba8c34bc2680b6f19bd90a4d0038ec5bc00
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-06-11 23:41:35 +0200

    Changed toast behaviour to trigger them from the frontend aswell

--- resources/views/components/toast-container.blade.php
+++ resources/views/components/toast-container.blade.php
@@ -1,5 +1,13 @@
 @foreach(App\Enums\ToastType::cases() as $type)
     @if(session($type->value))
-        <x-toast :type="$type"/>
+        <x-toast :type="$type" :message="session($type->value)"/>
     @endif
 @endforeach
+<x-toast :type="'info'" :message="''"/>
+<script>
+    function triggerToast(type, message) {
+        window.dispatchEvent(new CustomEvent('toast-trigger', {
+            detail: { type, message }
+        }));
+    }
+</script>
