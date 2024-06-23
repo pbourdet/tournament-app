4426464ebc751e986275c2ac4e44225c5276c6e1
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2024-06-23 20:50:53 +0200

    Create queued job to generate tournament teams

---
+++ tests/Unit/Jobs/GenerateTeamsTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Jobs;
+
+use App\Jobs\GenerateTeams;
+use App\Models\Team;
+use App\Models\Tournament;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Tests\TestCase;
+
+class GenerateTeamsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testItGeneratesTheTeams(): void
+    {
+        $tournament = Tournament::factory()
+            ->teamBased()
+            ->full()
+            ->create(['number_of_players' => 6]);
+
+        Team::factory()->withMembers($tournament->players()->take(2)->get())->create([
+            'tournament_id' => $tournament->id,
+            'name' => 'created team',
+        ]);
+
+        $lock = Cache::lock(sprintf('tournament:%s:generate-teams', $tournament->id), 20);
+        $lock->get();
+
+        (new GenerateTeams($tournament))->handle();
+
+        $this->assertDatabaseCount('teams', 3);
+
+        $teams = $tournament->teams;
+        $this->assertContains('Team 2', $teams->pluck('name')->toArray());
+        $this->assertContains('Team 3', $teams->pluck('name')->toArray());
+        foreach ($teams as $team) {
+            $this->assertCount(2, $team->members);
+        }
+        $this->assertSame(0, $tournament->players()->whereDoesntHave('teams')->count());
+
+        $this->assertTrue($lock->get());
+    }
+
+    public function testItDoesNotCreateTeamsIsTournamentIsNotFull(): void
+    {
+        $tournament = Tournament::factory()->teamBased()->create();
+        $lock = Cache::lock(sprintf('tournament:%s:generate-teams', $tournament->id), 20);
+        $lock->get();
+
+        (new GenerateTeams($tournament))->handle();
+
+        $this->assertDatabaseCount('teams', 0);
+        $this->assertTrue($lock->get());
+    }
+
+    public function testItDoesNotCreateTeamsIsTournamentIsTeamBased(): void
+    {
+        $tournament = Tournament::factory()->full()->create();
+        $lock = Cache::lock(sprintf('tournament:%s:generate-teams', $tournament->id), 20);
+        $lock->get();
+
+        (new GenerateTeams($tournament))->handle();
+
+        $this->assertDatabaseCount('teams', 0);
+        $this->assertTrue($lock->get());
+    }
+}
