3e88feefd31bd8005e5f836c6b98a248d67e8775
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2024-11-20 14:06:40 +0100

    Create livewire component for teams display

---
+++ app/Livewire/Tournament/Teams.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Tournament;
+
+use App\Enums\ToastType;
+use App\Jobs\GenerateTeams;
+use App\Models\Tournament;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Livewire\Component;
+
+class Teams extends Component
+{
+    public Tournament $tournament;
+
+    public function mount(Tournament $tournament): void
+    {
+        $this->tournament = $tournament;
+    }
+
+    public function boot(): void
+    {
+        $this->tournament->load(['teams.members']);
+    }
+
+    /** @return array<string, string> */
+    public function getListeners(): array
+    {
+        return [
+            sprintf('echo-private:App.Models.User.%s,TeamsGenerated', Auth::user()?->id) => 'generationDone',
+        ];
+    }
+
+    public function generate(): void
+    {
+        $this->authorize('manage', $this->tournament);
+
+        if (!$this->tournament->canGenerateTeams()) {
+            abort(403);
+        }
+
+        $this->checkLock($this->tournament);
+
+        GenerateTeams::dispatch($this->tournament, app()->getLocale());
+        $this->dispatch('toast-trigger', type: ToastType::INFO->value, message: __('Teams generation in progress'));
+    }
+
+    public function generationDone(): void
+    {
+        $this->tournament->load(['teams.members']);
+        $this->dispatch('toast-trigger', type: ToastType::SUCCESS->value, message: __('Teams generation done for tournament :tournament !', ['tournament' => $this->tournament->name]));
+    }
+
+    private function checkLock(Tournament $tournament): void
+    {
+        $lock = Cache::lock(sprintf('tournament:%s:generate-teams', $tournament->id), 20);
+
+        if (!$lock->get()) {
+            abort(409);
+        }
+    }
+}
---
+++ tests/Feature/Livewire/Tournament/TeamsTest.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Livewire\Tournament;
+
+use App\Events\TeamsGenerated;
+use App\Livewire\Tournament\Teams;
+use App\Models\Tournament;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Event;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class TeamsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Event::fake();
+    }
+
+    public function testRendersSuccessfully(): void
+    {
+        $tournament = Tournament::factory()->withAllTeams()->create();
+
+        Livewire::test(Teams::class, ['tournament' => $tournament])
+            ->assertStatus(200);
+    }
+
+    public function testOrganizerCanGenerateTeams(): void
+    {
+        $tournament = Tournament::factory()->full()->teamBased()->create();
+
+        Livewire::actingAs($tournament->organizer)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertSuccessful()
+            ->assertDispatched('toast-trigger');
+
+        Event::assertDispatched(TeamsGenerated::class);
+    }
+
+    public function testNonOrganizerCantGenerateTeams(): void
+    {
+        $user = User::factory()->create();
+        $tournament = Tournament::factory()->teamBased()->full()->create();
+
+        Livewire::actingAs($user)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertForbidden()
+            ->assertNotDispatched('toast-trigger');
+
+        Event::assertNotDispatched(TeamsGenerated::class);
+    }
+
+    public function testOrganizerCantGenerateTeamsOnNonFullTournament(): void
+    {
+        $tournament = Tournament::factory()->teamBased()->create();
+
+        Livewire::actingAs($tournament->organizer)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertForbidden()
+            ->assertNotDispatched('toast-trigger');
+
+        Event::assertNotDispatched(TeamsGenerated::class);
+    }
+
+    public function testOrganizerCantGenerateTeamsOnNonTeamBasedTournament(): void
+    {
+        $tournament = Tournament::factory()->full()->create();
+
+        Livewire::actingAs($tournament->organizer)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertForbidden()
+            ->assertNotDispatched('toast-trigger');
+
+        Event::assertNotDispatched(TeamsGenerated::class);
+    }
+
+    public function testOrganizerCantGenerateTeamsOnTournamentWithAllTeams(): void
+    {
+        $tournament = Tournament::factory()->full()->create();
+
+        Livewire::actingAs($tournament->organizer)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertForbidden()
+            ->assertNotDispatched('toast-trigger');
+
+        Event::assertNotDispatched(TeamsGenerated::class);
+    }
+
+    public function testOrganizerCantGenerateTeamsOnTournamentIfGenerationAlreadyOngoing(): void
+    {
+        $tournament = Tournament::factory()->full()->teamBased()->create();
+
+        Cache::lock(sprintf('tournament:%s:generate-teams', $tournament->id), 20)->get();
+
+        Livewire::actingAs($tournament->organizer)
+            ->test(Teams::class, ['tournament' => $tournament])
+            ->call('generate')
+            ->assertConflict()
+            ->assertNotDispatched('toast-trigger');
+
+        Event::assertNotDispatched(TeamsGenerated::class);
+    }
+}
