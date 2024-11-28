1ed7aaec6808dad1076d52adae639c3335e196bf
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-11-28 23:07:30 +0100

    Removed previous code and updated browser tests

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Enums\ToastType;
-use App\Http\Requests\TournamentStoreRequest;
 use App\Models\Tournament;
-use App\Models\TournamentInvitation;
 use App\Models\User;
 use App\Notifications\PlayerJoined;
 use Illuminate\Http\RedirectResponse;
@@ -33,41 +31,7 @@ class TournamentController extends Controller
         return redirect()->back()->with(ToastType::SUCCESS->value, __('You joined tournament :name', ['name' => $tournament->name]));
     }
 
-    public function create(): View|RedirectResponse
-    {
-        if (!Gate::allows('create', Tournament::class)) {
-            return redirect()->back()->with(ToastType::DANGER->value, __('You cannot create more tournaments'));
-        }
-
-        return view('tournaments.create');
-    }
-
-    public function store(TournamentStoreRequest $request): RedirectResponse
-    {
-        Gate::authorize('create', Tournament::class);
-
-        /** @var User $user */
-        $user = $request->user();
-
-        $tournament = Tournament::create([
-            'organizer_id' => $user->id,
-            'name' => $request->name,
-            'number_of_players' => $request->number_of_players,
-            'description' => $request->description,
-            'team_based' => $request->boolean('team_based'),
-            'team_size' => $request->boolean('team_based') ? $request->integer('team_size') : null,
-        ]);
-
-        TournamentInvitation::fromTournament($tournament);
-
-        if ($request->boolean('join_tournament')) {
-            $tournament->players()->attach($user);
-        }
-
-        return redirect()->route('tournaments.show', ['tournament' => $tournament])->with(ToastType::SUCCESS->value, __('Tournament :name created !', ['name' => $tournament->name]));
-    }
-
-    public function show(Tournament $tournament, User $user): View
+    public function show(Tournament $tournament): View
     {
         Gate::authorize('view', $tournament);
 
