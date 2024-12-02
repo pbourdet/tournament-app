2c2dbb1dd714e98142229fb2ac816b84a0205d48
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-12-02 19:33:30 +0100

    Removed htmx dependency and updated npm packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,7 @@
 import './bootstrap';
-import htmx from 'htmx.org';
 import.meta.glob([
     '../images/**',
 ]);
-
-window.htmx = htmx
 
 if (import.meta.env.VITE_INCLUDE_JQUERY === 'true') {
     import('jquery').then(({ default: $ }) => {
