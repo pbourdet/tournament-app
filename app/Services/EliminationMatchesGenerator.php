12fd5cccaa75f48a8e1b294c3535b0bed1946761
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-12-11 21:48:18 +0100

    Updated match service to add player and added tests

--- app/Services/EliminationMatchesGenerator.php
+++ app/Services/EliminationMatchesGenerator.php
@@ -5,19 +5,29 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Models\EliminationPhase;
+use App\Models\Round;
 
 class EliminationMatchesGenerator
 {
     public function generate(EliminationPhase $phase): void
     {
-        foreach ($phase->rounds as $round) {
+        $contestants = $phase->tournament->contestants()->shuffle();
+        $rounds = $phase->rounds
+            ->sortByDesc(fn (Round $round) => $round->stage->getMatchCount())
+            ->values();
+
+        foreach ($rounds as $key => $round) {
             $matchesCount = $round->stage->getMatchCount();
 
             for ($i = 0; $i < $matchesCount; ++$i) {
-                $round->matches()->create([
+                $match = $round->matches()->create([
                     'tournament_id' => $phase->tournament->id,
                     'index' => $i + 1,
                 ]);
+
+                if (0 === $key) { // First round
+                    $match->contestants()->attach($contestants->splice(0, 2));
+                }
             }
         }
     }
