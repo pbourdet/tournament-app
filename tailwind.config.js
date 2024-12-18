c6746f7e7d1a768c72ff563e770e0b2b45c739e0
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2024-12-18 13:31:45 +0100

    Installed teal flux theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -19,6 +20,14 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                zinc: colors.gray,
+                accent: {
+                    DEFAULT: 'var(--color-accent)',
+                    content: 'var(--color-accent-content)',
+                    foreground: 'var(--color-accent-foreground)',
+                },
+            },
             fontFamily: {
                 sans: ['Inter', 'sans-serif'],
             },
