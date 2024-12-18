baac5d5db0bde80bebf51074d1ec2f7535fa56e2
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2024-12-18 14:03:31 +0100

    Reworked guest layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.base')
 
-@section('body-class', 'font-sans antialiased')
-@section('container-class', 'min-h-screen bg-gray-100')
 @section('content')
     <div>
         {{ $slot }}
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.base')
 
-@section('body-class', 'font-sans text-gray-900 antialiased')
-@section('container-class', 'min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100')
-
 @section('content')
-    <div class="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="flex flex-col items-center pt-[10vh]">
         {{ $slot }}
     </div>
 @endsection
