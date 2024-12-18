57cc5fa1bd51c262d8bdbe0d1c4b22413875434a
Pierre Bourdet <pierre.bourdet1@gmail.com>
2024-12-18 20:59:46 +0100

    Removed previous login code

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -12,39 +12,6 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testLoginScreenCanBeRendered(): void
-    {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
-        $response->assertSee('Register');
-    }
-
-    public function testUsersCanAuthenticateUsingTheLoginScreen(): void
-    {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
-    }
-
-    public function testUsersCanNotAuthenticateWithInvalidPassword(): void
-    {
-        $user = User::factory()->create();
-
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
-
-        $this->assertGuest();
-    }
-
     public function testUsersCanLogout(): void
     {
         $user = User::factory()->create();
