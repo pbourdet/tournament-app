fc0a7710234d6292de553315c9e5d52df816b3dc
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2025-01-20 22:00:38 +0100

    Removed useless components

--- resources/views/components/close-modal-button.blade.php
+++
@@ -1,8 +0,0 @@
-<div @click="$dispatch('close')"
-     class="cursor-pointer text-gray-500 hover:bg-gray-200 hover:text-gray-900 rounded-md w-8 h-8 inline-flex justify-center items-center">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-         stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
-    </svg>
-    <span class="sr-only">Close modal</span>
-</div>
