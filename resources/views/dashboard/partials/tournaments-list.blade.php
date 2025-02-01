e9006a5e279c1d24a83e48f8db3caba1771f3414
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2025-02-01 18:16:49 +0100

    Change dashboard link to organizer page if logged as organizer

--- resources/views/dashboard/partials/tournaments-list.blade.php
+++ resources/views/dashboard/partials/tournaments-list.blade.php
@@ -4,7 +4,11 @@
     </h3>
     <div class="mt-2">
         @foreach ($tournaments as $tournament)
-            <a href="{{ route('tournaments.show', ['tournament' => $tournament, 'page' => 'overview']) }}" wire:navigate class="block border rounded-lg border-gray-200 hover:bg-gray-200 hover:shadow-md transition duration-150">
+            @can('manage', $tournament)
+                <a href="{{ route('tournaments.organize', ['tournament' => $tournament, 'page' => 'general']) }}" wire:navigate class="block border rounded-lg border-gray-200 hover:bg-gray-200 hover:shadow-md transition duration-150">
+            @else
+                <a href="{{ route('tournaments.show', ['tournament' => $tournament, 'page' => 'overview']) }}" wire:navigate class="block border rounded-lg border-gray-200 hover:bg-gray-200 hover:shadow-md transition duration-150">
+            @endcan
                 <div class="p-8 hover:bg-gray-100">
                     <h4 class="text-lg font-bold text-cyan-700 mb-2">{{ $tournament->name }}</h4>
                     <p class="text-sm text-gray-500">{{ $tournament->description }}</p>
