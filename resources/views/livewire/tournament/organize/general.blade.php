834020371fbb0fba87555f31e8438f11590da50d
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2025-03-10 13:31:38 +0100

    Hide start tournament card when started

--- resources/views/livewire/tournament/organize/general.blade.php
+++ resources/views/livewire/tournament/organize/general.blade.php
@@ -1,32 +1,34 @@
 <div>
-    <flux:card class="space-y-4 shadow lg:max-w-2/3">
-        <flux:heading>
-            {{ __('To start the tournament') }}
-        </flux:heading>
-        <ul class="text-sm space-y-2">
-            @include('livewire.tournament.partials.start-condition', [
-                'condition' => $tournament->isFull(),
-                'text' => __('The tournament must have all its players')
-            ])
-            @include('livewire.tournament.partials.start-condition', [
-                'condition' => $tournament->getPhases()->isNotEmpty(),
-                'text' => __('The tournament must have at least one phase (elimination or qualification)')
-            ])
-            @if(null !== $tournament->groupPhase)
+    @if($tournament->isNotStarted())
+        <flux:card class="space-y-4 shadow lg:max-w-2/3">
+            <flux:heading>
+                {{ __('To start the tournament') }}
+            </flux:heading>
+            <ul class="text-sm space-y-2">
                 @include('livewire.tournament.partials.start-condition', [
-                    'condition' => $tournament->groupPhase->isReadyToStart(),
-                    'text' => __('The groups must have all their :contestants', ['contestants' => $tournament->getContestantsTranslation(true)])
+                    'condition' => $tournament->isFull(),
+                    'text' => __('The tournament must have all its players')
                 ])
-            @endif
-            @if($tournament->team_based)
                 @include('livewire.tournament.partials.start-condition', [
-                    'condition' => $tournament->teams->every->isFull(),
-                    'text' => __('The teams must have all their members')
+                    'condition' => $tournament->getPhases()->isNotEmpty(),
+                    'text' => __('The tournament must have at least one phase (elimination or qualification)')
                 ])
-            @endif
-        </ul>
-        <flux:button dusk="start-tournament" :disabled="$this->user->cannot('start', $tournament)" wire:click="start">
-            {{ __('Start tournament') }}
-        </flux:button>
-    </flux:card>
+                @if(null !== $tournament->groupPhase)
+                    @include('livewire.tournament.partials.start-condition', [
+                        'condition' => $tournament->groupPhase->isReadyToStart(),
+                        'text' => __('The groups must have all their :contestants', ['contestants' => $tournament->getContestantsTranslation(true)])
+                    ])
+                @endif
+                @if($tournament->team_based)
+                    @include('livewire.tournament.partials.start-condition', [
+                        'condition' => $tournament->teams->every->isFull(),
+                        'text' => __('The teams must have all their members')
+                    ])
+                @endif
+            </ul>
+            <flux:button dusk="start-tournament" :disabled="$this->user->cannot('start', $tournament)" wire:click="start">
+                {{ __('Start tournament') }}
+            </flux:button>
+        </flux:card>
+    @endif
 </div>
