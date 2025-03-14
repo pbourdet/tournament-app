55921b027f76213a8f14cd51f0fed986eedd656c
Pierre Bourdet <pierre.bourdet@envoituresimone.com>
2025-03-14 14:55:11 +0100

    Changed tournament event handling on front end

--- resources/views/livewire/tournament/organizer-zone.blade.php
+++ resources/views/livewire/tournament/organizer-zone.blade.php
@@ -48,15 +48,12 @@
         <livewire:is :component="sprintf('tournament.organize.%s', $page)" :$organizerMode :$tournament />
     </flux:main>
 
-    @script
-    <script>
-        window.Echo.private('App.Models.Tournament.{{ $tournament->id }}')
-            .listen('TournamentUpdated', function() {
-                let parentComponent = Livewire.getByName('tournament.organizer-zone')[0];
-                if (parentComponent === undefined) return;
-                let childComponent = Livewire.getByName('tournament.organize.'+ parentComponent.page)[0];
-                childComponent.$refresh();
-            });
+    <script data-navigate-once>
+        document.addEventListener('livewire:navigated', () => {
+            window.Echo.private('App.Models.Tournament.{{ $tournament->id }}')
+                .listen('TournamentUpdated', function() {
+                    Livewire.navigate(window.location.pathname + window.location.search + window.location.hash)
+                });
+        }, { once: true });
     </script>
-    @endscript
 </div>
